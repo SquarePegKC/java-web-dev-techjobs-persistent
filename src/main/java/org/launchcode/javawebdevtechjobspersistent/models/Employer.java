018536fd2d53a46204d203e7c4ee9ff7d1d1d0d7
SquarePegKC <56491947+SquarePegKC@users.noreply.github.com>
2020-03-07 15:16:36 -0600

    stage1

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,34 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.lang.reflect.Constructor;
+
+@Entity
+
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location required")
+    @Size(min = 3, max = 100)
+    private String location;
+
+   // Constructor
+    public Employer (String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    //Hibernate
+    public Employer (){
+
+    }
 
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,28 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
-public class Skill extends AbstractEntity {
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+@Entity
+public class Skill extends AbstractEntity {
+    @NotBlank
+    @Size (min = 3, max = 250)
+    private String description ;
+
+    //Constructor
+    public Skill (String description) {
+        this.description = description;
+    }
+    public Skill(){
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
